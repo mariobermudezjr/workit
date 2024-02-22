a16505167ae3bb8e34dd59158ce21894d446a64f
Mario Bermudez JR <mario.bermudez.jr@outlook.com>
2024-02-23 02:49:15 +0700

    Initialize repo

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import excercises from './assets/data/exercises.json'
 
 export default function App() {
+  console.log(excercises[0])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.exerciseContainer}>
+        <Text style={styles.maintext}>{excercises[0].name}</Text>
+        <Text style={styles.subtext}>
+          {excercises[0].muscle} | {excercises[0].equipment}{' '}
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gainsboro',
     justifyContent: 'center',
+    padding: 10,
   },
-});
+  exerciseContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 8,
+    gap: 2,
+  },
+  maintext: {
+    textTransform: 'capitalize',
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  subtext: {
+    color: 'dimgray',
+    textTransform: 'capitalize',
+  },
+})
